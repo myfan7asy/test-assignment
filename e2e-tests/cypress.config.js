50c0508750ec5ab2b359516f7d27906d76895ab4
Oleksandr Ramadanov <alexander.ramadanov@gmail.com>
2023-02-16 22:19:45 +0100

    Another set of changes to workflow file

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+})
+
+module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   watchForFileChanges: false,
