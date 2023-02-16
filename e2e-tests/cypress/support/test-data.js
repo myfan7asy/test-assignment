cd993bf82c91ec89413c8f7ff6493e7bb89cc11b
Oleksandr Ramadanov <alexander.ramadanov@gmail.com>
2023-02-16 23:19:08 +0100

    test commit

--- e2e-tests/cypress/support/test-data.js
+++ e2e-tests/cypress/support/test-data.js
@@ -1,13 +1,7 @@
 class TestData {
     feeds = [
-        ["http://feeds.mashable.com/Mashable", 1],
-        ["https://www.al-monitor.com/pulse/podcast/rss.xml", 1],
-        ["https://www.polygon.com/rss/index.xml", 1],
-        ["https://www.theverge.com/rss/index.xml", 1],
-        ["https://feeds.feedburner.com/techcrunch/startups", 1],
-        ["https://www.wired.com/feed/category/gear/latest/rss", 1],
-        ["https://www.xda-developers.com/feed/", 1],
-        ["https://www.ign.com/articles?tags=article&startIndex=0&count=20&format=rss", 1],
+        ["http://www.nu.nl/rss/Algemeen", 1],
+        ["https://feeds.feedburner.com/tweakers/mixed", 1],
         ["https://rss.art19.com/daily-tech-news-show", 1],
         ["https://feeds.npr.org/1001/rss.xml", 1],
         ["https://www.theguardian.com/world/rss", 1],
